55ec8a77bab64f58f274494b3846aaecf829f738
Alex Colombo <uricaine@hotmail.com>
2018-07-30 20:13:25 +0200

    cambio variable

---
+++ api/index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var app = require('./app')
+var port = 3600;
+
+//Conexion base de datos
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/peliculas', { useNewUrlParser: true })
+    .then(() => {
+        console.log('Conectado a Mongo base de datos Peliculas');
+
+        //Crear Servidor
+        app.listen(port, () => {
+            console.log(`Servidor corriendo en http://localhost: ${port}`);
+        })
+    })
+    .catch(err => console.log(err))
