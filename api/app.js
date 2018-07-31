ff5385a98c70be124bbe01923d82ac61e525bd53
Alex Colombo <uricaine@hotmail.com>
2018-07-31 21:04:45 +0200

    new feature in app.js

--- api/app.js
+++ api/app.js
@@ -9,8 +9,8 @@ var bodyParser = require('body-parser');
 var app = express();
 
 //Cargamos rutas
-
-//middlewares, es un metodo que se ejecuta antes de un controlador, en cada peticion se ejectutará este middleware
+var user_routes = require('./routes/user')
+    //middlewares, es un metodo que se ejecuta antes de un controlador, en cada peticion se ejectutará este middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 //convertimos todo los que nos llegue en json
 app.use(bodyParser.json())
@@ -18,13 +18,7 @@ app.use(bodyParser.json())
 //cors
 
 //rutas
-app.get('/', (req, res) => {
-    res.status(200).send({ message: "Pruebas en servidor NodeJs" })
-})
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.status(200).send({ message: "Pruebas en servidor NodeJs" })
-})
+app.use('/api', user_routes)
 
 //exportar (exportamos lo que app tenga)
 module.exports = app;
