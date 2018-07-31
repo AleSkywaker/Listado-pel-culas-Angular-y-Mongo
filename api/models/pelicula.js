0920dd16eb41b10f5335349dffff84954cb24600
Alex Colombo <uricaine@hotmail.com>
2018-07-31 21:05:38 +0200

    creado pelicula model

---
+++ api/models/pelicula.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let PeliculasSchema = Schema({
+    user: { type: Schema.ObjectId, ref: 'User' },
+    actors: String,
+    awards: String,
+    country: String,
+    fechaDvd: String,
+    director: String,
+    genre: String,
+    language: String,
+    plot: String,
+    poster: String,
+    production: String,
+    rated: String,
+    rating: Array,
+    release: String,
+    runtime: String,
+    title: String,
+    website: String,
+    writer: String,
+    year: String,
+    imdbID: String,
+    imdbRating: String,
+    imdbVotes: String
+})
+
+module.exports = mongoose.model('Pelicula', PeliculasSchema);
