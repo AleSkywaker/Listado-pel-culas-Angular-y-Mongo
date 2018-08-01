7ea1b2eb3d4ad989246f0a9dcf8508124c624b71
Alex Colombo <uricaine@hotmail.com>
2018-08-01 20:56:33 +0200

    cambio nombre variable userControllertroller

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,11 +1,11 @@
 'use strict'
 
 const express = require('express');
-const kaka = require('../controllers/user.js');
+const UserController = require('../controllers/user.js');
 
 const api = express.Router();
 
-api.get('/home', kaka.home);
-api.get('/pruebas', kaka.pruebas);
+api.get('/home', UserController.home);
+api.get('/pruebas', UserController.pruebas);
 
 module.exports = api;
