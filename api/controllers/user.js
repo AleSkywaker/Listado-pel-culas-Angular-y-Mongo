97ab6df3482d606a21abbfca008fc360bbbf08b0
Alex Colombo <uricaine@hotmail.com>
2018-08-02 19:55:24 +0200

    creado funcion saveuser

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -11,7 +11,20 @@ function pruebas(req, res) {
     res.status(200).send({ message: "Pruebas en servidor NodeJs" })
 }
 
+function saveUser(req, res) {
+    let params = req.body;
+    // let user = new User();
+    if (params.password === params.password2) {
+        res.status(200).send({ message: "Las contraseñas son iguales" })
+    } else {
+        res.status(200).send({ message: "Las contraseñas NO son iguales" })
+    }
+
+
+}
+
 module.exports = {
     home,
-    pruebas
+    pruebas,
+    saveUser
 }
