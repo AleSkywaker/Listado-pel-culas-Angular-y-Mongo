9acf2dc36722386d192b7742efcd42630c1926e7
Alex Colombo <uricaine@hotmail.com>
2018-08-02 19:55:40 +0200

    creado ruta para saveuser

--- api/routes/user.js
+++ api/routes/user.js
@@ -7,5 +7,6 @@ const api = express.Router();
 
 api.get('/home', UserController.home);
 api.get('/pruebas', UserController.pruebas);
+api.post('/guardarusuario', UserController.saveUser);
 
 module.exports = api;
