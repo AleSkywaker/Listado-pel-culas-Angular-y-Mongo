80b4fbbed520967136234da670afbff58d0db91d
Alex Colombo <uricaine@hotmail.com>
2018-08-02 19:57:40 +0200

    cleaning user routes

--- api/routes/user.js
+++ api/routes/user.js
@@ -5,8 +5,6 @@ const UserController = require('../controllers/user.js');
 
 const api = express.Router();
 
-api.get('/home', UserController.home);
-api.get('/pruebas', UserController.pruebas);
 api.post('/guardarusuario', UserController.saveUser);
 
 module.exports = api;
