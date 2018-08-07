603055e449c4855ca879c0dbf646b6e7246fb97f
Alex Colombo <uricaine@hotmail.com>
2018-08-07 19:47:20 +0200

    añadido ruta para login user

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,5 +6,6 @@ const UserController = require('../controllers/user.js');
 const api = express.Router();
 
 api.post('/save-user', UserController.saveUser);
+api.post('/login', UserController.loginUser);
 
 module.exports = api;
