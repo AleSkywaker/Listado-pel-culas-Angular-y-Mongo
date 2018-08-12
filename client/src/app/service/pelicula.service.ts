514386e582e754e1740d36c9eca4c9040a4c2290
Alex Colombo <uricaine@hotmail.com>
2018-08-12 12:26:56 +0200

    probando peliculas

--- client/src/app/service/pelicula.service.ts
+++ client/src/app/service/pelicula.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+
 
 @Injectable()
-export class PeliculaService {
+export class PeliculaService implements OnInit {
 
+  public apikey = environment.apikey;
   constructor() { }
+
+  ngOnInit() {
+    console.log(this.apikey);
+  }
 
 }
