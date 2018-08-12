e162f56407cceeac8ccaa37f09049d41471c6003
Alex Colombo <uricaine@hotmail.com>
2018-08-12 12:58:36 +0200

    cambios import peliculas ts

--- client/src/app/service/pelicula.service.ts
+++ client/src/app/service/pelicula.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnInit } from '@angular/core';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environmentt';
 
 
 
