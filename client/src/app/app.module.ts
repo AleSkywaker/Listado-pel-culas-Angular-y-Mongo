e53a8da2cc8afd43fa84e6e6a85c97ab61a2471a
Alex Colombo <uricaine@hotmail.com>
2018-08-13 23:21:20 +0200

    servicios añadidos en app.module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+// Importar HttpClientModule
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BuscadorComponent } from './buscador/buscador.component';
+import { PeliculaService } from './service/pelicula.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuscadorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule, // cargamos el módulo en el array de imports
+    FormsModule
   ],
-  providers: [],
+  providers: [PeliculaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
