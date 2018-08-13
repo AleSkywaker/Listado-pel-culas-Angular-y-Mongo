d56408c9e246bbc9538dadcd83291a2c63202fb3
Alex Colombo <uricaine@hotmail.com>
2018-08-13 23:20:23 +0200

    creado buscador component

---
+++ client/src/app/buscador/buscador.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { PeliculaService } from '../service/pelicula.service';
+
+@Component({
+  selector: 'app-buscador',
+  templateUrl: './buscador.component.html',
+  styleUrls: ['./buscador.component.css']
+})
+export class BuscadorComponent implements OnInit {
+
+  public peliculas = [];
+
+  constructor(private _peliService: PeliculaService) { }
+
+  ngOnInit() {
+  }
+
+  onFormSubmit(f) {
+    let pelicula = f.value.pelis;
+    this._peliService.getPeliculas(pelicula).subscribe(data => {
+      this.peliculas = data.Search;
+      console.log("Peliculas", this.peliculas)
+    })
+
+  }
+}
