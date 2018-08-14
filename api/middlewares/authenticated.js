6964649f528379059c8c6dbac2cc675786a7994f
Alex Colombo <uricaine@hotmail.com>
2018-08-14 10:36:15 +0200

    comprobando si tiene cabeceras de authoriaztion

--- api/middlewares/authenticated.js
+++ api/middlewares/authenticated.js
@@ -3,3 +3,11 @@
 var jwt = require('jwt-simple');
 var moment = require('moment');
 var secret = process.env.CLAVE_SECRETA
+
+exports.ensureAuth = function(req, res, next) {
+
+    if (!req.headers.authorization) {
+        res.status(403).send({ message: "La cabecera no tiene authorazación" })
+    }
+
+}
