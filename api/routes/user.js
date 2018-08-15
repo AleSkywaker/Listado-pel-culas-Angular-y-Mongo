d99f724733463c229b3166d032bb2aadf8727917
Alex Colombo <uricaine@hotmail.com>
2018-08-15 10:00:28 +0200

    añadido middleware en rutas

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,11 +1,14 @@
 'use strict'
 
 const express = require('express');
-const UserController = require('../controllers/user.js');
+const UserController = require('../controllers/user');
+
+const md_auth = require('../middlewares/authenticated')
 
 const api = express.Router();
 
 api.post('/save-user', UserController.saveUser);
 api.post('/login', UserController.loginUser);
+api.get('/prueba', md_auth.ensureAuth, UserController.pruebas);
 
 module.exports = api;
