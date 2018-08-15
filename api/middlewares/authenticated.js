bd11c86a148a022d9f45b281e65df453acf6ef80
Alex Colombo <uricaine@hotmail.com>
2018-08-15 10:01:35 +0200

    añadido metodo para verificar token

--- api/middlewares/authenticated.js
+++ api/middlewares/authenticated.js
@@ -10,6 +10,23 @@ exports.ensureAuth = function(req, res, next) {
         res.status(403).send({ message: "La cabecera no tiene authorazación" })
     }
 
-    let token = req.headers.authorization.replace(/['"]/ + g, '')
+    let token = req.headers.authorization.replace(/['"]+/g, '')
+
+    try {
+        var payload = jwt.decode(token, secret)
+        if (payload.exp <= moment().unix()) {
+            return res.status(401).send({
+                message: 'El token ha expirado'
+            })
+        }
+    } catch (error) {
+        return res.status(403).send({
+            message: 'El token no es válido'
+        })
+    }
+
+    req.user = payload
+
+    next()
 
 }
