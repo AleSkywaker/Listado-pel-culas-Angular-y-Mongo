a33ae4ecbc5de898bb997c955eeb6f69c22be1d1
Alex Colombo <uricaine@hotmail.com>
2018-08-17 17:43:02 +0200

    añadido rutas

--- api/routes/user.js
+++ api/routes/user.js
@@ -10,5 +10,7 @@ const api = express.Router();
 api.post('/save-user', UserController.saveUser);
 api.post('/login', UserController.loginUser);
 api.get('/prueba', md_auth.ensureAuth, UserController.pruebas);
+api.get('/get-user/:id', md_auth.ensureAuth, UserController.getUser);
+api.get('/users/:page?', md_auth.ensureAuth, UserController.getUsers);
 
 module.exports = api;
