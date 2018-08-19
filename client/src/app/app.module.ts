f8ad65c93745413e799038a389475e141295042c
Alex Colombo <uricaine@hotmail.com>
2018-08-19 12:12:12 +0200

    añadido nuevo component (pelicula)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,23 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+//Rutas
+import { APP_ROUITNG } from './app.routes'
+
 // Importar HttpClientModule
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BuscadorComponent } from './buscador/buscador.component';
 import { PeliculaService } from './service/pelicula.service';
+import { PeliculaComponent } from './pelicula/pelicula.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BuscadorComponent
+    BuscadorComponent,
+    PeliculaComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule, // cargamos el módulo en el array de imports
-    FormsModule
+    FormsModule,
+    APP_ROUITNG
   ],
   providers: [PeliculaService],
   bootstrap: [AppComponent]
