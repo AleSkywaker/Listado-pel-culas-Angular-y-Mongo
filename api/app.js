18669ec3bd994530a2dbb3af05d17879eba9afbe
Alex Colombo <uricaine@hotmail.com>
2018-08-20 10:38:28 +0200

    se ñade cors

--- api/app.js
+++ api/app.js
@@ -10,15 +10,25 @@ var app = express();
 
 //Cargamos rutas
 var user_routes = require('./routes/user')
+var pelicula_routes = require('./routes/pelicula')
     //middlewares, es un metodo que se ejecuta antes de un controlador, en cada peticion se ejectutará este middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 //convertimos todo los que nos llegue en json
 app.use(bodyParser.json())
 
 //cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    next();
+});
 
 //rutas
 app.use('/api', user_routes)
+app.use('/api', pelicula_routes)
 
 //exportar (exportamos lo que app tenga)
 module.exports = app;
