72afd80f418901237c9fb0f1cf636b1a84a49151
Alex Colombo <uricaine@hotmail.com>
2018-08-20 10:42:00 +0200

    inyeccion de servicio para traer detalles de la pelicula

--- client/src/app/pelicula/pelicula.component.ts
+++ client/src/app/pelicula/pelicula.component.ts
@@ -1,3 +1,4 @@
+import { PeliculaService } from './../service/pelicula.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
@@ -7,14 +8,42 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./pelicula.component.css']
 })
 export class PeliculaComponent implements OnInit {
+  public pelicula: any;
 
-  constructor(private _activatedRoute: ActivatedRoute) {
+  constructor(private _activatedRoute: ActivatedRoute,
+    private _peliculaService: PeliculaService) {
     this._activatedRoute.params.subscribe(params => {
-      console.log(params);
+      this._peliculaService.getDetallePelicula(params.id).subscribe(data => {
+        this.pelicula = data;
+        if (this.pelicula.Type == 'series') {
+          this.pelicula.Type = 'de la serie'
+        }
+        if (this.pelicula.Type == 'movie') {
+          this.pelicula.Type = 'de la pelicula'
+        }
+        if (this.pelicula.Type == 'game') {
+          this.pelicula.Type = 'del juego'
+        }
+        console.log("Constructor", this.pelicula);
+      })
+
     })
   }
 
   ngOnInit() {
   }
+  grabarPeli(id) {
+    this._peliculaService.getDetallePelicula(id).subscribe(data => {
+      this.pelicula = data;
+      console.log("grabarPeli", this.pelicula);
+
+      this._peliculaService.guardarPelicula(this.pelicula).subscribe(data => {
+        console.log("que es::", data);
+      })
+
+    })
+  }
 
 }
+
+
