f22e401a2cef2242f2cacc41f949ef3bde748f0d
Alex Colombo <uricaine@hotmail.com>
2018-08-21 11:00:08 +0200

    se crea modelo Peli para vista

---
+++ client/src/app/models/pelicula.ts
@@ -0,0 +1,34 @@
+export class Pelicula {
+  constructor(
+    public _id: String,
+    public text: String,
+    public file: String,
+    public create_at: String,
+    public user: String
+  ) { }
+}
+
+// Actors : "Michael Keaton, Jack Nicholson, Kim Basinger, Robert Wuhl"
+// Awards : "Won 1 Oscar. Another 8 wins & 26 nominations."
+// BoxOffice : "N/A"
+// Country : "USA, UK"
+// DVD : "25 Mar 1997"
+// Director : "Tim Burton"
+// Genre : "Action, Adventure"
+// Language : "English, French, Spanish"
+// Metascore : "69"
+// Plot : "The Dark Knight of Gotham City begins his war on crime with his first major enemy being the clownishly homicidal Joker."
+// Poster : "https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg"
+// Production : "Warner Bros. Pictures"
+// Rated : "PG-13"
+// Released : "23 Jun 1989"
+// Response : "True"
+// Runtime : "126 min"
+// Title : "Batman"
+// Type : "movie"
+// Website : "N/A"
+// Writer : "Bob Kane (Batman characters), Sam Hamm (story), Sam Hamm (screenplay), Warren Skaaren (screenplay)"
+// Year : "1989"
+// imdbID : "tt0096895"
+// imdbRating : "7.6"
+// imdbVotes : "300,704"
