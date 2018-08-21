d0c0e6ccfa5f7baa76f8d243f6fb534ff0f64cdb
Alex Colombo <uricaine@hotmail.com>
2018-08-21 11:01:16 +0200

    se crea metodo para guardar peli desde la vista

--- client/src/app/pelicula/pelicula.component.ts
+++ client/src/app/pelicula/pelicula.component.ts
@@ -9,6 +9,8 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PeliculaComponent implements OnInit {
   public pelicula: any;
+  public message: String;
+  public status: String;
 
   constructor(private _activatedRoute: ActivatedRoute,
     private _peliculaService: PeliculaService) {
@@ -32,18 +34,23 @@ export class PeliculaComponent implements OnInit {
 
   ngOnInit() {
   }
-  grabarPeli(id) {
+  grabarPeli(id, puntos) {
+    console.log("puntos", puntos)
+    if (puntos == undefined || puntos == "" || puntos == null) {
+      this.status = "error";
+      this.message = "Debes puntuar la pelicula antes de guardar"
+      return
+    }
     this._peliculaService.getDetallePelicula(id).subscribe(data => {
       this.pelicula = data;
-      console.log("grabarPeli", this.pelicula);
-
+      this.pelicula.puntos = puntos;
       this._peliculaService.guardarPelicula(this.pelicula).subscribe(data => {
         console.log("que es::", data);
       })
-
+      this.status = "success";
+      this.message = "La pelicula se ha guardado correctamente!!!"
     })
   }
-
 }
 
 
