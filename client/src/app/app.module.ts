6c0c433c55483af71acabfde9b72640b1ba33db3
Alex Colombo <uricaine@hotmail.com>
2018-08-25 10:51:40 +0200

    deleted modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatTableModule, MatSortModule, MatPaginatorModule } from '@angular/material'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 //Rutas
 import { APP_ROUITNG } from './app.routes'
@@ -15,25 +13,20 @@ import { BuscadorComponent } from './buscador/buscador.component';
 import { PeliculaService } from './service/pelicula.service';
 import { PeliculaComponent } from './pelicula/pelicula.component';
 import { MispeliculasComponent } from './mispeliculas/mispeliculas.component';
-import { DataTableComponent } from './component/data-table/data-table.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     BuscadorComponent,
     PeliculaComponent,
-    MispeliculasComponent,
-    DataTableComponent
+    MispeliculasComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule, // cargamos el módulo en el array de imports
     FormsModule,
-    APP_ROUITNG,
-    MatTableModule,
-    MatSortModule,
-    BrowserAnimationsModule,
-    MatPaginatorModule
+    APP_ROUITNG
   ],
   providers: [PeliculaService],
   bootstrap: [AppComponent]
