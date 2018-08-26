c1fe09f4d66825e377da586137fd1caffc994fe8
Alex Colombo <uricaine@hotmail.com>
2018-08-26 14:31:39 +0200

    add methods to mispeliculas component

--- client/src/app/mispeliculas/mispeliculas.component.ts
+++ client/src/app/mispeliculas/mispeliculas.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
 import { PeliculaService } from '../service/pelicula.service';
-import { MatSort, MatSortable, MatTableDataSource } from '@angular/material';
+import { Router } from '@angular/router';
+
+import { Subject } from 'rxjs';
+import 'rxjs/add/operator/map';
+import { DOCUMENT } from '@angular/common';
+
 
 @Component({
   selector: 'app-mispeliculas',
@@ -8,27 +13,50 @@ import { MatSort, MatSortable, MatTableDataSource } from '@angular/material';
   styleUrls: ['./mispeliculas.component.css']
 })
 
-export class MispeliculasComponent implements OnInit {
-  @ViewChild(MatSort) sort: MatSort;
+export class MispeliculasComponent implements OnDestroy, OnInit {
+  dtOptions: DataTables.Settings = {};
   mispelis;
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource;
-
-  constructor(private _peliculaService: PeliculaService) {
+  // We use this trigger because fetching the list of persons can be quite long,
+  // thus we ensure the data is fetched before rendering
+  dtTrigger: Subject<any> = new Subject();
+  constructor(private _peliculaService: PeliculaService,
+    private _router: Router,
+    @Inject(DOCUMENT) private document: any) {
   }
 
   ngOnInit() {
-    this.getPelis()
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 5,
+    }
+    this._peliculaService.getMisPeliculas().subscribe(data => {
+      if (data) {
+        this.mispelis = data.pelisbuenas;
+        console.log(this.mispelis);
+        // Calling the DT trigger to manually render the table
+        this.dtTrigger.next();
+      }
+    });
+  };
+  ngOnDestroy(): void {
+    // Do not forget to unsubscribe the event
+    this.dtTrigger.unsubscribe();
   }
+
 
   getPelis() {
     this._peliculaService.getMisPeliculas().subscribe(data => {
       if (data) {
-        // this.mispelis = data.pelisbuenas;
-        this.dataSource = data.pelisbuenas;
-        this.dataSource.sort = this.sort;
+        this.mispelis = data.pelisbuenas;
         console.log(this.mispelis);
       }
     });
+  }
+
+  hazalgo(id) {
+    // this.document.location.href = "https://www.imdb.com/title/" + id;
+
+    this._router.navigate(['/pelicula', id])
+
   }
 }
