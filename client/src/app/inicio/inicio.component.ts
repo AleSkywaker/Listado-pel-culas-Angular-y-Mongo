f440f4efb1a7787a244d4cc14b0a8838215377d7
Alex Colombo <uricaine@hotmail.com>
2018-08-29 10:10:31 +0200

    creado inicio component

---
+++ client/src/app/inicio/inicio.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
+})
+export class InicioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  abrirSidebar() {
+    $('#sidebar').toggleClass('active');
+    $(this).toggleClass('active');
+  }
+
+}
