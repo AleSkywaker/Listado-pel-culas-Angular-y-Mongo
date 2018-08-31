905c79fa930b4ceacb0ea361afe89dd0bffc6f0a
Alex Colombo <uricaine@hotmail.com>
2018-08-31 09:44:43 +0200

    add delete method in pelicula controller

--- api/controllers/pelicula.js
+++ api/controllers/pelicula.js
@@ -8,7 +8,7 @@ function grabarPeli(req, res) {
 
     let aux = (params.puntos / 10) * 100;
 
-    let pelicula = new Pelicula();
+    var pelicula = new Pelicula();
     pelicula.porcentaje = (aux / 10) * 10;
     pelicula.actors = params.Actors;
     pelicula.awards = params.Awards;
@@ -32,17 +32,37 @@ function grabarPeli(req, res) {
     pelicula.imdbVotes = params.imdbVotes;
     pelicula.points = params.puntos;
 
-    pelicula.save((err, peliculaStored) => {
-        if (err) return res.status(500).send({
-            message: "Error al guardar la pelicula"
+    if (pelicula.points == null || pelicula.points == undefined || pelicula.points == "") {
+        return res.status(402).send({
+            message: 'Debes indicar tu puntos',
         })
-        if (!peliculaStored) return res.status(404).send({
-            message: "La pelicula no ha sido guardada"
+    }
+    if (pelicula.points < 0 || pelicula.points > 10) {
+        return res.status(402).send({ message: 'Los puntos deben estar entre 0 y 10' })
+    }
+
+    Pelicula.find({ imdbID: pelicula.imdbID })
+        .exec((err, peli) => {
+            if (err) {
+                return res.status(406).send({ message: "Error al encontrar pelicula" })
+            }
+            if (peli && peli.length >= 1) {
+                return res.status(405).send({ message: "!NO SE HA GUARDADO ESTA PELICULA¡ dado que esta pelicula ya se encuentra en su lista personal" })
+            }
+            //Guarda pelicula
+            pelicula.save((err, peliculaStored) => {
+                if (err) return res.status(500).send({
+                    message: "Error al guardar la pelicula"
+                })
+                if (!peliculaStored) return res.status(404).send({
+                    message: "La pelicula no ha sido guardada"
+                })
+                return res.status(200).send({
+                    pelicula: peliculaStored,
+                    message: "Pelicula guardada correctamente!!"
+                })
+            })
         })
-        return res.status(200).send({
-            pelicula: peliculaStored
-        })
-    })
 }
 
 function getMovies(req, res) {
@@ -58,7 +78,21 @@ function getMovies(req, res) {
         })
 }
 
+function deleteMovie(req, res) {
+    let movieID = req.params.id;
+
+    Pelicula.find({ 'imdbID': movieID }).remove(
+        (err) => {
+            if (err) return res.status(500).send({
+                message: "Error al eliminar publicacion"
+            })
+            return res.status(200).send({
+                message: "La publicacion ha sido eliminada"
+            })
+        })
+}
 module.exports = {
     grabarPeli,
-    getMovies
+    getMovies,
+    deleteMovie
 }
