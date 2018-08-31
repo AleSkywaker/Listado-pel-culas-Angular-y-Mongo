a31cdf0bb7232afddad51c8a206d09233e63dbd0
Alex Colombo <uricaine@hotmail.com>
2018-08-31 09:47:45 +0200

    change style in inicio component

--- client/src/app/inicio/inicio.component.ts
+++ client/src/app/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import * as $ from 'jquery';
 
 @Component({
@@ -8,14 +9,41 @@ import * as $ from 'jquery';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  public tooltip: boolean;
+  public tooltipName;
+
+  constructor(
+    private _router: Router) {
+    this.tooltip = true;
+    this.tooltipName = "cerrar"
+  }
 
   ngOnInit() {
+
   }
 
   abrirSidebar() {
+    this.tooltip ? this.tooltip = false : this.tooltip = true;
+    if (this.tooltip) {
+      this.tooltipName = "cerrar"
+    } else {
+      this.tooltipName = "abrir"
+    }
+
     $('#sidebar').toggleClass('active');
+    $('#sidebarCollapse').toggleClass('active');
     $(this).toggleClass('active');
+    console.log("object", this.tooltip);
+  }
+
+  onFormSubmit(f) {
+    let pelicula = f.value.nombrePeli;
+    this._router.navigate(['/listado', pelicula])
+  }
+
+  detalles(id) {
+    this._router.navigate(['/pelicula', id])
+    console.log("hola", id);
   }
 
 }
