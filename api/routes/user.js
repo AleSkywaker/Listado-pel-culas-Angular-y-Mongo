856301d34415a475a9b102174cec824677af3400
Alex Colombo <uricaine@hotmail.com>
2018-09-08 11:31:48 +0200

    (feature): new method added to  user route

--- api/routes/user.js
+++ api/routes/user.js
@@ -7,10 +7,11 @@ const md_auth = require('../middlewares/authenticated')
 
 const api = express.Router();
 
+api.get('/prueba', md_auth.ensureAuth, UserController.pruebas);
 api.post('/save-user', UserController.saveUser);
 api.post('/login', UserController.loginUser);
-api.get('/prueba', md_auth.ensureAuth, UserController.pruebas);
 api.get('/get-user/:id', md_auth.ensureAuth, UserController.getUser);
 api.get('/users/:page?', md_auth.ensureAuth, UserController.getUsers);
+api.put('/update-user/:id', md_auth.ensureAuth, UserController.updateUser)
 
 module.exports = api;
