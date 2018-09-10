0a7e8b0efed8a190954ae37c464811693f8a431e
Alex Colombo <uricaine@hotmail.com>
2018-09-10 19:49:26 +0200

    new routes added

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,19 +1,20 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 
-
-import { PeliculaComponent } from './pelicula/pelicula.component';
-import { MispeliculasComponent } from './mispeliculas/mispeliculas.component';
-import { ListadoPelisComponent } from './listado-pelis/listado-pelis.component';
-import { MiPeliculaComponent } from './mi-pelicula/mi-pelicula.component';
-
-
+import { PeliculaComponent } from './components/pelicula/pelicula.component';
+import { MispeliculasComponent } from './components/mispeliculas/mispeliculas.component';
+import { ListadoPelisComponent } from './components/listado-pelis/listado-pelis.component';
+import { MiPeliculaComponent } from './components/mi-pelicula/mi-pelicula.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: 'pelicula/:id', component: PeliculaComponent },
   { path: 'mipelicula/:id', component: MiPeliculaComponent },
   { path: 'listado/:pelicula', component: ListadoPelisComponent },
   { path: 'mispeliculas', component: MispeliculasComponent },
+  { path: 'login', component: LoginComponent },
+  { path: "register", component: RegisterComponent },
   { path: "**", component: MispeliculasComponent }
 
   // { path: '**', component: PageNotFoundComponent },
