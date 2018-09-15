6ba23d5ada3747c562d937c5a601d9d2d3119197
Alex Colombo <uricaine@hotmail.com>
2018-09-15 10:52:59 +0200

    new properties added in login component

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -12,6 +12,11 @@ import { UserService } from '../../service/user.service';
 export class LoginComponent implements OnInit {
   public titulo: String;
   public user: User;
+  public status: String;
+  public message: String;
+  public identity;
+  public token;
+
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
@@ -19,13 +24,57 @@ export class LoginComponent implements OnInit {
 
   ) {
     this.titulo = "Identificate";
-    this.user = new User("", "", "", "", "", "", "", "", "", "", "", "ROLE_USER")
+    this.user = new User("", "", "", "", "", "", "", "", "", "", "", "ROLE_USER", "")
   }
 
   ngOnInit() {
     console.log("Login cargado")
   }
   onSubmit() {
-    console.log("login user", this.user);
+    //logear usuario y conseguir datos
+    this._userService.singUp(this.user).subscribe(response => {
+      this.identity = response.user;
+      if (!this.identity || !this.identity._id) {
+        this.status = "error";
+        this.message = "Usuario no existe o contraseña invalida"
+      } else {
+        this.status = "success";
+        //TODO: Persistir datos en localStorage
+        //TODO: Conseguir Token
+        this.getToken();
+        this.message = response.message;
+      }
+    },
+      error => {
+        var errorMessage = <any>error;
+        console.log("errores", errorMessage)
+        if (errorMessage = ! null) {
+          this.status = "error";
+          this.message = error.error.message;
+        }
+      })
+  }
+
+  getToken() {
+    this._userService.singUp(this.user, 'true').subscribe(response => {
+      this.token = response.token;
+      if (this.token.length <= 0) {
+        this.status = "error";
+      } else {
+        this.status = "success";
+        //TODO: Persistir token del usuario
+        //TODO: Conseguir las estadisticas del usuario
+        this.message = response.message;
+      }
+    },
+      error => {
+        var errorMessage = <any>error;
+        console.log("errores", errorMessage)
+        if (errorMessage = ! null) {
+          this.status = "error";
+          this.message = error.error.message;
+        }
+      })
+
   }
 }
