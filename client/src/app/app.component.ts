440bdced1377793ed1f99c9d0236cc6f9a58a493
Alex Colombo <uricaine@hotmail.com>
2018-09-16 11:49:18 +0200

    use localstorage property

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { UserService } from './service/user.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public titulo: String;
+  public identity;
+
+  constructor(private _userService: UserService) {
+    this.titulo = "Peliculas"
+  }
+
+  ngOnInit() {
+    this.identity = this._userService.getIdentity();
+    console.log("Peliculas", this.identity)
+  }
 }
