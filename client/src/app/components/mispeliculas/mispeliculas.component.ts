163937426824e3819ef645fded052458358b7896
Alex Colombo <uricaine@hotmail.com>
2018-09-22 11:30:57 +0200

    change links

--- client/src/app/components/mispeliculas/mispeliculas.component.ts
+++ client/src/app/components/mispeliculas/mispeliculas.component.ts
@@ -60,7 +60,7 @@ export class MispeliculasComponent implements OnDestroy, OnInit {
 
   hazalgo(id) {
     // this.document.location.href = "https://www.imdb.com/title/" + id;
-    this._router.navigate(['/mipelicula', id])
+    this._router.navigate(['/inicio/mipelicula', id])
   }
 
   borrarPeli(id) {
