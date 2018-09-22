b8174bf02c47036308475a3bf56a364a536636ad
Alex Colombo <uricaine@hotmail.com>
2018-09-22 11:10:08 +0200

    refactor

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { UserService } from './service/user.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
   providers: [UserService]
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, DoCheck {
 
+  public title: String;
+  public identity;
 
   constructor(private _userService: UserService) {
+    this.title = "Red Social";
 
   }
 
   ngOnInit() {
-
+    this.identity = this._userService.getIdentity();
+    console.log("identidad desde app.component", this.identity);
+  }
+  ngDoCheck() {
+    this.identity = this._userService.getIdentity();
+    console.log("algo cambio", this.identity);
   }
 }
