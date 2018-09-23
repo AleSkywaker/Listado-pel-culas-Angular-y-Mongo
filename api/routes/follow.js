7ad6b57eb480e68d60180037890d25d84edeb9fb
Alex Colombo <uricaine@hotmail.com>
2018-09-23 09:58:03 +0200

    follow routes

---
+++ api/routes/follow.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const express = require('express');
+const FollowController = require('../controllers/follow');
+const api = express.Router();
+const md_auth = require('../middlewares/authenticated')
+
+api.post('/seguir-usuario', md_auth.ensureAuth, FollowController.seguirUsuario);
+api.delete('/dejardeseguir/:id', md_auth.ensureAuth, FollowController.deleteFollow);
+api.get('/siguiendo/:id?/:page?', md_auth.ensureAuth, FollowController.getFollowingUsers);
+
+
+module.exports = api;
