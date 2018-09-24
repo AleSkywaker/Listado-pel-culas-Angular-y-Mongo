542100bc822a756d920a1d1aacef5fada49aafce
Alex Colombo <uricaine@hotmail.com>
2018-09-24 20:04:12 +0200

    mejoras en pelicula router

--- api/routes/pelicula.js
+++ api/routes/pelicula.js
@@ -2,14 +2,13 @@
 
 const express = require('express');
 const PeliculaController = require('../controllers/pelicula');
-
-// const md_auth = require('../middlewares/authenticated')
+const md_auth = require('../middlewares/authenticated');
 
 const api = express.Router();
 
-api.post('/grabarpeli', PeliculaController.grabarPeli);
+api.post('/grabarpeli', md_auth.ensureAuth, PeliculaController.grabarPeli);
 api.delete('/eliminarpeli/:id', PeliculaController.deleteMovie);
-api.get('/pelis', PeliculaController.getMovies);
+api.get('/pelis', md_auth.ensureAuth, PeliculaController.getMovies);
 api.get('/pelicula/:id', PeliculaController.getMyMovie);
 
 
