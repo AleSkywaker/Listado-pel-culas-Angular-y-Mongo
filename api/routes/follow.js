2f085029aac59d82e522a20ffcd38c3cb75d0055
Alex Colombo <uricaine@hotmail.com>
2018-09-25 20:49:36 +0200

    new method route

--- api/routes/follow.js
+++ api/routes/follow.js
@@ -8,6 +8,7 @@ const md_auth = require('../middlewares/authenticated')
 api.post('/seguir-usuario', md_auth.ensureAuth, FollowController.seguirUsuario);
 api.delete('/dejardeseguir/:id', md_auth.ensureAuth, FollowController.deleteFollow);
 api.get('/siguiendo/:id?/:page?', md_auth.ensureAuth, FollowController.getFollowingUsers);
+api.get('/siguido/:id?/:page?', md_auth.ensureAuth, FollowController.getFollowedUsers);
 
 
 module.exports = api;
