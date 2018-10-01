185273e5155c2850b1bcf4de7cf4e131195ca611
Alex Colombo <uricaine@hotmail.com>
2018-10-01 22:03:42 +0200

    save publication added

--- api/routes/publication.js
+++ api/routes/publication.js
@@ -2,10 +2,13 @@
 
 const express = require('express');
 const PublicationController = require('../controllers/publication');
+const api = express.Router();
 const md_auth = require('../middlewares/authenticated');
 
-const api = express.Router();
+const multipart = require('connect-multiparty');
+const md_upload = multipart({ uploadDir: './uploads/publications' })
 
 api.get('/probando-publi', md_auth.ensureAuth, PublicationController.probando);
+api.post('/guardar-publi', md_auth.ensureAuth, PublicationController.savePublication);
 
 module.exports = api;
