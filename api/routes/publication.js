8975d0abe94e711b299e817fab51fe1c97ad8899
Alex Colombo <uricaine@hotmail.com>
2018-10-01 22:18:29 +0200

    new route added

--- api/routes/publication.js
+++ api/routes/publication.js
@@ -8,7 +8,7 @@ const md_auth = require('../middlewares/authenticated');
 const multipart = require('connect-multiparty');
 const md_upload = multipart({ uploadDir: './uploads/publications' })
 
-api.get('/probando-publi', md_auth.ensureAuth, PublicationController.probando);
 api.post('/guardar-publi', md_auth.ensureAuth, PublicationController.savePublication);
+api.get('/get-publications/:page?', md_auth.ensureAuth, PublicationController.getPublications);
 
 module.exports = api;
