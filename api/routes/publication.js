3868d02c1b57333d4bc6d85b613de72d5b9aa484
Alex Colombo <uricaine@hotmail.com>
2018-10-03 22:12:52 +0200

    fix routes

--- api/routes/publication.js
+++ api/routes/publication.js
@@ -9,7 +9,7 @@ const multipart = require('connect-multiparty');
 const md_upload = multipart({ uploadDir: './uploads/publications' })
 
 api.post('/guardar-publi', md_auth.ensureAuth, PublicationController.savePublication);
-api.get('/get-publications/:page?', md_auth.ensureAuth, PublicationController.getPublications);
-api.get('/get-publications/:id', md_auth.ensureAuth, PublicationController.getPublication);
+api.get('/publications/:page?', md_auth.ensureAuth, PublicationController.getPublications);
+api.get('/publication/:id', md_auth.ensureAuth, PublicationController.getPublication);
 
 module.exports = api;
