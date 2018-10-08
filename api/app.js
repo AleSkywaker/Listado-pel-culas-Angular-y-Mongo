bd004e2671b8e1e0a2c3b2c4cea9d18c0e7c14d2
Alex Colombo <uricaine@hotmail.com>
2018-10-08 22:10:01 +0200

    importing new route

--- api/app.js
+++ api/app.js
@@ -13,6 +13,7 @@ var user_routes = require('./routes/user')
 var pelicula_routes = require('./routes/pelicula')
 var follow_routes = require('./routes/follow')
 var publication_routes = require('./routes/publication')
+var message_route = require('./routes/message')
     //middlewares, es un metodo que se ejecuta antes de un controlador, en cada peticion se ejectutará este middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 //convertimos todo los que nos llegue en json
@@ -33,6 +34,7 @@ app.use('/api', user_routes)
 app.use('/api', pelicula_routes)
 app.use('/api', follow_routes)
 app.use('/api', publication_routes)
+app.use('/api', message_route)
 
 //exportar (exportamos lo que app tenga)
 module.exports = app;
