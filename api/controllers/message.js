66c83bdabbddcecc210dd6cd6aefd91947fb4985
Alex Colombo <uricaine@hotmail.com>
2018-10-08 22:05:06 +0200

    create test method

--- api/controllers/message.js
+++ api/controllers/message.js
@@ -1,4 +1,5 @@
 'use strict'
+import * as module from 'module';
 
 const moment = require('moment');
 const mongoosePaginate = require('mongoose-pagination');
@@ -6,3 +7,12 @@ const mongoosePaginate = require('mongoose-pagination');
 const User = require('../models/user');
 const Follow = require('../models/follow');
 const Message = require('../models/message');
+
+
+function prueba(req, res) {
+    return res.status(200).send({ message: "probando" })
+}
+
+module.exports = {
+    prueba
+}
