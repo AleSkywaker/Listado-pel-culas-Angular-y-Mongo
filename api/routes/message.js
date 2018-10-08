d7c7d1765300e4bda749f5c18e85035d49c925e9
Alex Colombo <uricaine@hotmail.com>
2018-10-08 22:12:03 +0200

    fix new route

--- api/routes/message.js
+++ api/routes/message.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const md_auth = require('../middlewares/authenticated');
-const MessageController = requrie('../controllers//message.js');
+const MessageController = require('../controllers//message.js');
 
 const api = express.Router();
 
