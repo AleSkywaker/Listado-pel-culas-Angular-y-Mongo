1b5a06ff0f852d7a9981536714282b70b6f0b1a6
Alex Colombo <uricaine@hotmail.com>
2018-10-08 22:02:04 +0200

    importing libraries

--- api/controllers/message.js
+++ api/controllers/message.js
@@ -1,1 +1,8 @@
 'use strict'
+
+const moment = require('moment');
+const mongoosePaginate = require('mongoose-pagination');
+
+const User = require('../models/user');
+const Follow = require('../models/follow');
+const Message = require('../models/message')
