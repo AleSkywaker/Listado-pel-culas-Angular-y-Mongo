90ddeddb4af055fd63336e3f65a2ca543c85c0e5
Alex Colombo <uricaine@hotmail.com>
2018-10-10 22:52:00 +0200

    add new route

--- api/routes/message.js
+++ api/routes/message.js
@@ -6,6 +6,8 @@ const MessageController = require('../controllers//message.js');
 
 const api = express.Router();
 
-api.get('/probandocontroller', MessageController.prueba)
+api.get('/probandocontroller', MessageController.prueba);
+api.post('/save-message', md_auth.ensureAuth, MessageController.saveMessage)
+api.get('/messages/:page?', md_auth.ensureAuth, MessageController.getReceiverMessages)
 
 module.exports = api;
