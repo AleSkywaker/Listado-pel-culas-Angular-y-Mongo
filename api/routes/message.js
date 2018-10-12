14434a20c6f931e920696b86dde6e22f1bc61082
Alex Colombo <uricaine@hotmail.com>
2018-10-12 09:38:17 +0200

    fix routing

--- api/routes/message.js
+++ api/routes/message.js
@@ -7,7 +7,7 @@ const MessageController = require('../controllers//message.js');
 const api = express.Router();
 
 // api.get('/probandocontroller', MessageController.prueba);
-// api.post('/save-message', md_auth.ensureAuth, MessageController.saveMessage)
+api.post('/save-message', md_auth.ensureAuth, MessageController.saveMessage)
 api.get('/messages/:page?', md_auth.ensureAuth, MessageController.getReceiverMessages)
 
 module.exports = api;
