112a069f754577983cbd92647ffae64e17ffd26f
Alex Colombo <uricaine@hotmail.com>
2018-10-12 09:39:54 +0200

    CLEANING routing

--- api/routes/message.js
+++ api/routes/message.js
@@ -6,7 +6,6 @@ const MessageController = require('../controllers//message.js');
 
 const api = express.Router();
 
-// api.get('/probandocontroller', MessageController.prueba);
 api.post('/save-message', md_auth.ensureAuth, MessageController.saveMessage)
 api.get('/messages/:page?', md_auth.ensureAuth, MessageController.getReceiverMessages)
 
