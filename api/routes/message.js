ea928adf4b08aaa7e265ea8bf53f2288724d5120
Alex Colombo <uricaine@hotmail.com>
2018-10-13 19:42:02 +0200

    creating route for getEmitterMessages

--- api/routes/message.js
+++ api/routes/message.js
@@ -7,6 +7,7 @@ const MessageController = require('../controllers//message.js');
 const api = express.Router();
 
 api.post('/save-message', md_auth.ensureAuth, MessageController.saveMessage)
-api.get('/messages/:page?', md_auth.ensureAuth, MessageController.getReceiverMessages)
+api.get('/mensajes-recibidos/:page?', md_auth.ensureAuth, MessageController.getReceiverMessages);
+api.get('/mensajes-emitidos/:page?', md_auth.ensureAuth, MessageController.getEmitMessages);
 
 module.exports = api;
