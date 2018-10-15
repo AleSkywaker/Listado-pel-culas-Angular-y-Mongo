6eee57b6d974b0704fb7d3bdacd5ac57c9e01cfe
Alex Colombo <uricaine@hotmail.com>
2018-10-15 19:26:52 +0200

    route for movie followed

--- api/routes/pelicula.js
+++ api/routes/pelicula.js
@@ -10,6 +10,7 @@ api.post('/grabarpeli', md_auth.ensureAuth, PeliculaController.grabarPeli);
 api.delete('/eliminarpeli/:id', PeliculaController.deleteMovie);
 api.get('/pelis', md_auth.ensureAuth, PeliculaController.getMovies);
 api.get('/pelicula/:id', PeliculaController.getMyMovie);
+api.get('/peliculas-seguidos/:id', md_auth.ensureAuth, PeliculaController.getMoviesSeguido);
 
 
 module.exports = api;
