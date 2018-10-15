95dbb18c940d95c0bf24d471cd6a1d9b91db4270
Alex Colombo <uricaine@hotmail.com>
2018-10-15 19:26:33 +0200

    route for set message as viweded

--- api/routes/message.js
+++ api/routes/message.js
@@ -10,5 +10,6 @@ api.post('/save-message', md_auth.ensureAuth, MessageController.saveMessage)
 api.get('/mensajes-recibidos/:page?', md_auth.ensureAuth, MessageController.getReceiverMessages);
 api.get('/mensajes-emitidos/:page?', md_auth.ensureAuth, MessageController.getEmitMessages);
 api.get('/mensajes-no-leidos', md_auth.ensureAuth, MessageController.getMessagesNoLeidos);
+api.get('/marcar-mensajes-leidos', md_auth.ensureAuth, MessageController.setViewedMessages);
 
 module.exports = api;
