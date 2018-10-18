3db5b5cc6fda2f65df5d47a0cfd62dc63c0e2ecb
Alex Colombo <uricaine@hotmail.com>
2018-10-18 19:28:35 +0200

    starting controller

--- client/src/app/components/users/users.component.ts
+++ client/src/app/components/users/users.component.ts
@@ -1,13 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { UserService } from '../../service/user.service';
+import { User } from '../../models/users';
+import { GLOBAL } from './../../service/global';
+
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
+  providers: [UserService]
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  public title: string;
+  public identity;
+  public token;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _userService: UserService
+  ) {
+    this.title = 'Gente';
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+  }
 
   ngOnInit() {
   }
