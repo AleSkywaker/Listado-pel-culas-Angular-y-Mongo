abcb51d5d971a24cdfa25b425808d435c2fb0da1
Alex Colombo <uricaine@hotmail.com>
2018-10-21 11:33:16 +0200

    fix user model

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -24,7 +24,7 @@ export class RegisterComponent implements OnInit {
     private _userService: UserService
   ) {
     this.titulo = "Registrate"
-    this.user = new User("", "", "", "", "", "", "", "", "", "", "", "ROLE_USER", "")
+    this.user = new User("", "", "", "", "", "", "", "", "", "", "", "ROLE_USER", "", "")
   }
 
   ngOnInit() {
