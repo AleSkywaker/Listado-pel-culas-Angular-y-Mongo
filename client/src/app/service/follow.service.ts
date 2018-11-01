172ce9233cb035d1f42c223fadcb8b5899541fb8
Alex Colombo <uricaine@hotmail.com>
2018-11-01 22:41:12 +0100

    test code

--- client/src/app/service/follow.service.ts
+++ client/src/app/service/follow.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { environment } from '../../environments/environmentt';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -8,12 +8,15 @@ import { Follow } from '../models/follow';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
-export class FollowService {
+export class FollowService implements OnInit {
 
   public url: string;
 
   constructor(private _http: HttpClient) {
-
+    this.url = GLOBAL.url;
   }
-
+  ngOnInit(): void {
+    console.log("follow component load");
+    // throw new Error("Method not implemented.");
+  }
 }
