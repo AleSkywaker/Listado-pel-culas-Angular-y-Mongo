6f4cc2325e8dcc92e446e2224f302abdc7516c42
Alex Colombo <uricaine@hotmail.com>
2018-11-01 22:36:39 +0100

    inyect follow model in follow service

--- client/src/app/service/follow.service.ts
+++ client/src/app/service/follow.service.ts
@@ -4,7 +4,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { GLOBAL } from './global';
 import { User } from '../models/users';
+import { Follow } from '../models/follow';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
-export class FollowService implements OnInit { }
+export class FollowService implements OnInit {
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+  }
+}
