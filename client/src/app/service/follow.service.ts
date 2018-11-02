79c1ef7b308e808bd47f4d68992b6e84acb919b0
Alex Colombo <uricaine@hotmail.com>
2018-11-02 08:09:37 +0100

    dinyect follow service in u

--- client/src/app/service/follow.service.ts
+++ client/src/app/service/follow.service.ts
@@ -27,7 +27,7 @@ export class FollowService implements OnInit {
     return this._http.post(this.url + '/seguir-usuario', params, { headers: headers });
   }
 
-  deleteFollow(token, id) {
+  deleteFollow(token, id): Observable<any> {
     let headers = new HttpHeaders().set('Content-Type', 'application/json')
       .set('Authorization', token)
 
