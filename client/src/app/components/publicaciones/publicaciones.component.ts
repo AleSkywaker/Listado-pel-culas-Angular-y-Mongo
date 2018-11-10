60113c0c83a54319d71b21510d189b9d05641f1c
Alex Colombo <uricaine@hotmail.com>
2018-11-10 09:22:12 +0100

    inyect publication service

--- client/src/app/components/publicaciones/publicaciones.component.ts
+++ client/src/app/components/publicaciones/publicaciones.component.ts
@@ -3,10 +3,12 @@ import { GLOBAL } from './../../service/global';
 import { PeliculaService } from './../../service/pelicula.service';
 import { UserService } from '../../service/user.service';
 import { Publication } from '../../models/publication';
+import { PublicationService } from '../../service/publication.service';
 @Component({
   selector: 'app-publicaciones',
   templateUrl: './publicaciones.component.html',
-  styleUrls: ['./publicaciones.component.css']
+  styleUrls: ['./publicaciones.component.css'],
+  providers: [PublicationService]
 })
 export class PublicacionesComponent implements OnInit {
   public identity;
@@ -18,7 +20,8 @@ export class PublicacionesComponent implements OnInit {
   public publication: Publication;
   constructor(
     private _userService: UserService,
-    private _peliculaService: PeliculaService
+    private _peliculaService: PeliculaService,
+    private _publicationService: PublicationService
   ) {
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
@@ -28,17 +31,51 @@ export class PublicacionesComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.getMyFavoriteMovie()
+    this.getCounters()
+  }
+  getMyFavoriteMovie() {
     this._peliculaService.miMejorPelicula(this.token).subscribe(
       response => {
         this.peliFavorita = response.mipelifavorita[0];
-        console.log("desde controller", this.peliFavorita);
       }
     )
   }
-  onSubmit() {
+  onSubmit(form) {
     console.log(this.publication);
+    this._publicationService.addPublication(this.token, this.publication).subscribe(
+      response => {
+        if (response.publicacion) {
+          this.publication = response.publicacion;
+          form.reset();
+        } else {
+          this.status = 'error';
+          setTimeout(() => {
+            this.status = ''
+          }, 1000);
+        }
+        this.status = 'success'
+        setTimeout(() => {
+          this.status = ''
+        }, 1000);
+      }, error => {
+        var errorMessage = <any>error;
+        console.log(errorMessage)
+        if (errorMessage != null) {
+          this.status = 'error';
+          setTimeout(() => {
+            this.status = ''
+          }, 1000);
+        }
+      }
+    )
   }
 
-
-
+  getCounters() {
+    this._userService.getCounters().subscribe((response) => {
+      localStorage.setItem('stats', JSON.stringify(response))
+    }, (error) => {
+      console.log(<any>error)
+    })
+  }
 }
