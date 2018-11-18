6fd7cbc13768a56b3f320a7f494c1261ae375105
Alex Colombo <uricaine@hotmail.com>
2018-11-18 12:11:31 +0100

    new method

--- client/src/app/components/profile/profile.component.ts
+++ client/src/app/components/profile/profile.component.ts
@@ -18,7 +18,7 @@ export class ProfileComponent implements OnInit {
   public status: string;
   public url: string;
   public token: string;
-  public identity: string;
+  public identity: User;
   public stats: string;
   public follow: string;
 
@@ -38,12 +38,50 @@ export class ProfileComponent implements OnInit {
   ngOnInit() {
     console.log("Profile cargado")
 
+    // this._route.params.subscribe(params => {
+    //   console.log("parametros", params)
+    //   this._userService.seeCompatibility(params.id).subscribe(data => {
+    //     console.log("que es data", data)
+    //   })
+    // })
+
+    this.loadPage();
+  }
+
+  loadPage() {
     this._route.params.subscribe(params => {
-      console.log("parametros", params)
-      this._userService.seeCompatibility(params.id).subscribe(data => {
-        console.log("que es data", data)
-      })
+      let id = params['id'];
+      this.getUser(id);
+      this.verCompatibilidad(id);
     })
+  }
+
+  getUser(id) {
+    this._userService.getUser(id).subscribe(
+      response => {
+        if (response.user) {
+          console.log(response)
+          this.user = response.user;
+        } else {
+          this.status = 'error'
+        }
+      }, error => {
+        console.log(<any>error)
+        this.status = 'error'
+        this._router.navigate(['/perfil', this.identity._id])
+      }
+    )
+  }
+
+  verCompatibilidad(id) {
+    this._userService.seeCompatibility(id).subscribe(
+      response => {
+        console.log("ESTO ES COMPATIBLIDAD", response)
+      }, error => {
+        console.log(<any>error)
+        this.status = 'error'
+      })
+
   }
 
 }
