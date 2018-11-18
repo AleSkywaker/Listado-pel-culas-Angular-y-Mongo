02238f345611bb00dce465bfcc01bc2bf2399e7c
Alex Colombo <uricaine@hotmail.com>
2018-11-18 18:03:20 +0100

    new urls in GLOBAL

--- client/src/app/service/global.ts
+++ client/src/app/service/global.ts
@@ -1,3 +1,5 @@
 export var GLOBAL = {
-  url: 'http://localhost:3600/api'
+  url: 'http://localhost:3600/api',
+  urlOMDB: 'http://www.omdbapi.com/?s=',
+  urlOMDBDetail: 'http://www.omdbapi.com/?i=',
 }
