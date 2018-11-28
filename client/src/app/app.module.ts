0e9d87b3a549251b3da84f449736d52f88228637
Alex Colombo <uricaine@hotmail.com>
2018-11-28 07:34:25 +0100

    add guard

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,36 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 //Rutas
-import { APP_ROUITNG } from './app.routes'
+import { APP_ROUITNG } from "./app.routes";
 
 // Importar HttpClientModule
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
 
 //DataTable
-import { DataTablesModule } from 'angular-datatables';
+import { DataTablesModule } from "angular-datatables";
 
 //Bootstrap
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
-import { AppComponent } from './app.component';
-import { PeliculaService } from './service/pelicula.service';
-import { PeliculaComponent } from './components/pelicula/pelicula.component';
-import { MispeliculasComponent } from './components/mispeliculas/mispeliculas.component';
-import { InicioComponent } from './components/inicio/inicio.component';
-import { ListadoPelisComponent } from './components/listado-pelis/listado-pelis.component';
-import { MiPeliculaComponent } from './components/mi-pelicula/mi-pelicula.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { UsersComponent } from './components/users/users.component';
-import { EditarperfilComponent } from './components/editarperfil/editarperfil.component';
-import { PublicacionesComponent } from './components/publicaciones/publicaciones.component';
-import { TimelineComponent } from './components/timeline/timeline.component';
-import { MomentModule } from 'angular2-moment';
-import { ProfileComponent } from './components/profile/profile.component';
+import { AppComponent } from "./app.component";
+import { PeliculaComponent } from "./components/pelicula/pelicula.component";
+import { MispeliculasComponent } from "./components/mispeliculas/mispeliculas.component";
+import { InicioComponent } from "./components/inicio/inicio.component";
+import { ListadoPelisComponent } from "./components/listado-pelis/listado-pelis.component";
+import { MiPeliculaComponent } from "./components/mi-pelicula/mi-pelicula.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { UsersComponent } from "./components/users/users.component";
+import { EditarperfilComponent } from "./components/editarperfil/editarperfil.component";
+import { PublicacionesComponent } from "./components/publicaciones/publicaciones.component";
+import { TimelineComponent } from "./components/timeline/timeline.component";
+import { MomentModule } from "angular2-moment";
+import { ProfileComponent } from "./components/profile/profile.component";
 
-
+//Servicios
+import { PeliculaService } from "./service/pelicula.service";
+import { UserService } from "./service/user.service";
+import { UserGuard } from "./service/user.guard";
 
 @NgModule({
   declarations: [
@@ -57,7 +59,7 @@ import { ProfileComponent } from './components/profile/profile.component';
     NgbModule,
     MomentModule
   ],
-  providers: [PeliculaService],
+  providers: [PeliculaService, UserService, UserGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
