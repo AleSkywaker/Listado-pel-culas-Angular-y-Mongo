1fb1d0d488cc063e634db848972877d37bfc52f1
Alex Colombo <uricaine@hotmail.com>
2018-12-03 19:47:57 +0100

    just testing

--- client/src/app/components/profile/chart.component.ts
+++ client/src/app/components/profile/chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 import * as d3 from "d3";
 
 @Component({
@@ -7,15 +7,19 @@ import * as d3 from "d3";
   styleUrls: ["./chart.component.css"]
 })
 export class ChartComponent implements OnInit {
+  @Input() id;
   constructor() {}
-  ngOnInit() {}
+
+  ngOnInit() {
+    // console.log("id desde ngAfterContentInit", this.id);
+  }
 
   ngAfterContentInit(): void {
     //Called after ngOnInit when the component's or directive's content has been initialized.
     //Add 'implements AfterContentInit' to the class.
-
     d3.select("h1").style("color", "red");
     d3.selectAll("rect:nth-child(even)").style("fill", "cyan");
+    console.log("id desde ngAfterContentInit", this.id);
   }
   doit() {
     d3.selectAll("rect").style("fill", "red");
