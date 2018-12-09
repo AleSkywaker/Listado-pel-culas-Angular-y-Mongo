017c132fd2ad9f99fc5518fbcdc4f4790a83887b
Alex Colombo <uricaine@hotmail.com>
2018-12-09 21:19:41 +0100

    testing

--- client/src/app/components/profile/chart.component.ts
+++ client/src/app/components/profile/chart.component.ts
@@ -9,23 +9,22 @@ import { UserService } from "./../../service/user.service";
 })
 export class ChartComponent implements OnInit {
   @Input() id;
-  public stats;
+  public data;
 
   constructor(private _userService: UserService) {}
 
   ngOnInit() {}
 
   ngAfterContentInit(): void {
+    this.getCounters(this.id);
     d3.select("h1").style("color", "red");
     d3.selectAll("rect:nth-child(even)").style("fill", "cyan");
-    this.getCounters(this.id);
   }
 
   getCounters(id) {
     this._userService.getCounters(id).subscribe(
       response => {
-        this.stats = response;
-        console.log("stats", this.stats);
+        this.data = response.stats;
       },
       error => {
         console.log(<any>error);
